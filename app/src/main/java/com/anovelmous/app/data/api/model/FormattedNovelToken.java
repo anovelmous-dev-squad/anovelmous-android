62100547e588fbf674f05720f52dd95cf0202487
Greg Ziegan <grz5@case.edu>
2015-06-02 10:28:02 +1200

    added FormattedNovelToken

---
+++ app/src/main/java/com/anovelmous/app/data/api/model/FormattedNovelToken.java
@@ -0,0 +1,69 @@
+package com.anovelmous.app.data.api.model;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+import org.joda.time.DateTime;
+
+import static com.anovelmous.app.util.Preconditions.checkNotNull;
+/**
+ * Created by Greg Ziegan on 6/2/15.
+ */
+public class FormattedNovelToken {
+    @NonNull public final String url;
+
+    @NonNull public final String content;
+
+    public final int ordinal;
+
+    @NonNull public final String chapter;
+
+    @SerializedName("created_at")
+    @NonNull public final DateTime createdAt;
+
+    public FormattedNovelToken(Builder builder) {
+        this.url = checkNotNull(builder.url, "url == null");
+        this.content = checkNotNull(builder.content, "content == null");
+        this.ordinal = builder.ordinal;
+        this.chapter = checkNotNull(builder.chapter, "chapter == null");
+        this.createdAt = checkNotNull(builder.createdAt, "createdAt == null");
+    }
+
+    public static final class Builder {
+        private String url;
+        private String content;
+        private int ordinal;
+        private String chapter;
+        private DateTime createdAt;
+
+        public Builder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder ordinal(int ordinal) {
+            this.ordinal = ordinal;
+            return this;
+        }
+
+        public Builder chapter(String chapter) {
+            this.chapter = chapter;
+            return this;
+        }
+
+        public Builder createdAt(DateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public FormattedNovelToken build() {
+            return new FormattedNovelToken(this);
+        }
+    }
+}

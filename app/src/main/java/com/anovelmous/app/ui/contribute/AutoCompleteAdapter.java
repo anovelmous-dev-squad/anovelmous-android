18477d36dce3fe601a68c56cd040bb69cea87b88
Greg Ziegan <grz5@case.edu>
2015-06-26 01:10:38 +1200

    naive, partially broken filter now in place

--- app/src/main/java/com/anovelmous/app/ui/contribute/AutoCompleteAdapter.java
+++ app/src/main/java/com/anovelmous/app/ui/contribute/AutoCompleteAdapter.java
@@ -2,7 +2,9 @@ package com.anovelmous.app.ui.contribute;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +15,7 @@ import rx.functions.Action1;
  */
 public class AutoCompleteAdapter extends ArrayAdapter<String> implements Action1<List<String>> {
     private List<String> items = Collections.emptyList();
+    final int LIMIT = 4;
 
     public AutoCompleteAdapter(Context context, int resource) {
         super(context, resource);
@@ -25,13 +28,56 @@ public class AutoCompleteAdapter extends ArrayAdapter<String> implements Action1
     }
 
     @Override
+    public void clear() {
+        items.clear();
+    }
+
+    @Override
     public int getCount() {
-        return items.size();
+        return Math.min(LIMIT, items.size());
     }
 
     @Override
     public String getItem(int index) {
         return items.get(index);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if(constraint != null) {
+                    List<String> filteredTokens = new ArrayList<>();
+                    for(String token : items){
+                        if(token.contains(constraint)){
+                            filteredTokens.add(token);
+                        }
+                    }
+                    // Now assign the values and count to the FilterResults object
+                    filterResults.values = filteredTokens;
+                    filterResults.count = filteredTokens.size();
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if(results != null && results.count > 0) {
+                    items = (List<String>) results.values;
+                    notifyDataSetChanged();
+                }
+                else {
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
     }
 
 }
